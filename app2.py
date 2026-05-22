c51de43869efcccc4617cea08404e7d81b2f6599
2802sophiav-dev <2802sophiav@gmail.com>
2026-05-21 23:44:59 -0500

    app2.py

--- app2.py
+++ app2.py
@@ -228,7 +228,7 @@ def ejecutar_modulo1():
             })
 
             st.success("Grupo agregado correctamente")
-            st.experimental_rerun()
+            st.rerun()
 
     with col_b2:
 
@@ -237,7 +237,7 @@ def ejecutar_modulo1():
             st.session_state.susts = []
 
             st.success("Estructura reiniciada")
-            st.experimental_rerun()
+            st.rerun()
 
     # =========================================================
     # MOSTRAR GRUPOS AGREGADOS
@@ -1127,7 +1127,7 @@ def ejecutar_modulo1():
                 reacciones_pool
             )
 
-            st.experimental_rerun()
+            st.rerun()
 
     # =========================================================
     # VALIDACIÓN
@@ -1334,7 +1334,7 @@ def ejecutar_modulo2():
                 st.session_state.idx_densidad + 1
             ) % len(ejercicios_densidad)
 
-            st.experimental_rerun()
+            st.rerun()
 
     # =========================================================
     # TAB 2: ESTABILIDAD DE INTERMEDIOS
@@ -1493,7 +1493,7 @@ def ejecutar_modulo2():
                 st.session_state.idx_intermedio + 1
             ) % len(ejercicios_intermedios)
 
-            st.experimental_rerun()
+            st.rerun()
 
 
 def ejecutar_modulo3():
@@ -2463,7 +2463,7 @@ def ejecutar_modulo3():
 
                             st.session_state.paso += 1
 
-                            st.experimental_rerun()
+                            st.rerun()
 
                         else:
 
@@ -2486,7 +2486,7 @@ def ejecutar_modulo3():
                     st.session_state.historial = []
                     st.session_state.feedback = ""
 
-                    st.experimental_rerun()
+                    st.rerun()
 
         # =====================================================
         # FEEDBACK
